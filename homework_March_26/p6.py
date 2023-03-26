ca65be9e36d5598df6280aedf41d34db7fd27d62
meerimkanybekova <kanybekova_m@auca.kg>
2023-03-26 17:00:13 +0600

    Add Problem #6

---
+++ homework_March_26/p6.py
@@ -0,0 +1,9 @@
+L = [-1, 5, -1, -2, -3, 4, 5, 6, -7, 8, 9, -10, 11, 12, -13, 14, 15, -16, 17, 18, 19]
+val = None
+
+for num in L:
+    if num % 2 != 0:
+        if val is None or num < val:
+            val = num
+            
+print(val)
