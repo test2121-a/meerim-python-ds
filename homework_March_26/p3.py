5846f5b4c053e9a620033b20af2d31a8628a3515
meerimkanybekova <kanybekova_m@auca.kg>
2023-03-26 16:26:17 +0600

    Add Problem #3

---
+++ homework_March_26/p3.py
@@ -0,0 +1,9 @@
+L = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+new_L = []
+
+for number in L:
+    if (number % 3 == 0) and (number % 5 == 0):
+
+        new_L.append(number * number)
+
+print(new_L)
